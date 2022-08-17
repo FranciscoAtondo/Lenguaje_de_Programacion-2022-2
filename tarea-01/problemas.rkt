eb7ab79bf262ad01c8da69cbdcfa7ad1a4d91e07
FranciscoAtondo <31047250+FranciscoAtondo@users.noreply.github.com>
2022-08-16 20:50:22 -0700

    Update problemas.rkt

--- tarea-01/problemas.rkt
+++ tarea-01/problemas.rkt
@@ -2,4 +2,11 @@
 
 ;; Escribe aquí tus soluciones
 
+(define (insertL x y lst)
+    )
+  
+(test-case "insertL"
+    (check-equal? (insertL 'x 'y '(x z z x y x))
+                  '(y x z z y x y y x)))
+
 (provide (all-defined-out))
